739ceb90145a1f134b1cfda04dec5db60e0c1daa
Venkat Kasagani <47335961+venkat-kasagani@users.noreply.github.com>
2022-07-30 23:59:01 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	static void test() {
+		System.out.println("test");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
